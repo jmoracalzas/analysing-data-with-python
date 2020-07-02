784ae70609a04eb8d4c0f3062666e1b3a17962a1
jmoracalzas <65016169+jmoracalzas@users.noreply.github.com>
2020-07-02 20:06:30 +0100

    Completing methods and attributes RE: Rules class

--- Project_1/dataGeneration.py
+++ Project_1/dataGeneration.py
@@ -1,33 +1,47 @@
 #!usr/bin/env python3
 
 #This part of the code will :
-    #Ask the user the number of year to generate transactions for
     #I will calculate the total number of transactions for income and expenditure
     #it will generate a list of transactions (income and expenditure) based the input obtained from the user
     #it will store the dataset
 
-from datetime import date
-
 class Rules:
     def __init__(self):
         self.__setNoYears = int(input("How many years does the data need to reflect?"))
-        self.setExpLines = int(input("How many lines of expenditure would you like to generate every month:"))
-        self.setIncLines =  int(input("How many lines of income would you like to generate every month:"))
+        self.__setIncLines =  int(input("How many lines of income would you like to generate every month:"))
+        self.__setExpLines = int(input("How many lines of expenditure would you like to generate every month:"))
 
-    def getNoYears(self):
-
+    def getNumYears(self, years):
         return years
+    
+    def getNumIncEntries(self,years, lines):
+        incLines = years * 12 * lines
+        return incLines
+
+    def getNumExpEntries(self,years,lines):
+        expLines = years * 12 * lines
+        return expLines
 
 def main():
+    newRules = Rules()
     
-    NewRules = Rules()
-    print(NewRules._Rules__seetNoYears)
-    #print(NewRules.getNoYears())
-    #print(NewRules.getNoYears())
-    #print(NewRules._Rules__setNoYears)
-    
-    
-    
+    #Generating the total number of income and expenditure lines
+    #The default values of the parameters shown in the getNumIncEntries and the getNumExpEntries refer to private variables initiated when the class Rules is instantiated
 
+    totalNumYears = newRules.getNumYears(newRules._Rules__setNoYears)
+
+    totalIncEntries = newRules.getNumIncEntries(
+        years = newRules._Rules__setNoYears,
+        lines = newRules._Rules__setIncLines
+    )
+    
+    totalExpLines = newRules.getNumExpEntries(
+        years = newRules._Rules__setNoYears,
+        lines = newRules._Rules__setExpLines
+    )
+
+    print(totalNumYears)
+    print(totalIncEntries)
+    print(totalExpLines)
+    
 if __name__=="__main__":    main()
-
