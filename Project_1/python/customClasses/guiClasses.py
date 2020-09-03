9632d5266ff71483aca070c5e76181668b292983
jmoracalzas <jordi.mora.calzas@outlook.com>
2020-09-03 10:54:13 +0100

    Creating the GUI

--- Project_1/python/customClasses/guiClasses.py
+++ Project_1/python/customClasses/guiClasses.py
@@ -7,26 +7,32 @@ class DataAnalysisApp:
     def __init__(self, master):
 
         # creating the menu frame and its content
-        menuFrame = ttk.Frame(master, width=25, relief=SOLID, border=5).grid(
-            row=0, column=0
-        )
+        menuFrame = ttk.Frame(master, width=100, height=200, border=1)
+        menuFrame.place(x=15, y=15)
 
-        actionButton = ttk.Button(menuFrame, text="Settings", padding=(2, 2)).grid(
-            row=0, column=0
-        )
+        settingsButton = ttk.Button(menuFrame, text="Settings")
+        settingsButton.pack(pady=5, padx=5)
 
-        closeButton = ttk.Button(menuFrame, text="Exit", padding=(2, 2)).grid(
-            row=1, column=0
-        )
+        getDataButton = ttk.Button(menuFrame, text="Get Data")
+        getDataButton.pack(padx=5, pady=5)
+
+        exportButton = ttk.Button(menuFrame, text="Export")
+        exportButton.pack(padx=5, pady=5)
+
+        closeButton = ttk.Button(master, text="Exit")
+        closeButton.place(x=15, y=240)
 
         # creating the display area text and its content
-        dispAreaFrame = ttk.Frame(master, height=50, width=200, relief=RIDGE).grid(
-            row=0, column=1, rowspan=2
-        )
+        dispAreaFrame = ttk.Frame(master, width=385, border=5, relief=SOLID)
+        dispAreaFrame.place(x=115, y=15)
 
         displayArea = ttk.Label(
-            dispAreaFrame, text="hello \world \world", justify="center"
-        ).grid(row=0, column=1, rowspan=2, sticky="nsew")
+            dispAreaFrame,
+            text="Work in progress... \n:)",
+            justify="center",
+            background="yellow",
+        )
+        displayArea.pack(fill="both")
 
 
 def main():
