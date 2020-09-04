52fe52f7573cf7d2da6ec6e4da857e9f3da0007e
jmoracalzas <jordi.mora.calzas@outlook.com>
2020-09-04 19:59:47 +0100

    Developing the Python GUI

--- Project_1/python/customClasses/guiClasses.py
+++ Project_1/python/customClasses/guiClasses.py
@@ -1,7 +1,3 @@
-from email import message
-from logging import root
-from sys import hexversion
-from textwrap import wrap
 from tkinter import *
 from tkinter import ttk
 
@@ -10,40 +6,48 @@ class DataAnalysisApp:
     def __init__(self, master):
 
         # creating the menu frame and its content
-        menuFrame = ttk.Frame(master, width=100, height=200, border=1)
-        menuFrame.place(x=15, y=15)
+        self.menuFrame = ttk.Frame(master, width=100, height=200, border=1)
+        self.menuFrame.place(x=15, y=15)
 
-        settingsButton = ttk.Button(menuFrame, text="Settings")
-        settingsButton.pack(pady=5, padx=5)
+        self.settingsButton = ttk.Button(
+            self.menuFrame, text="Settings", command=self.userInputLayout
+        )
+        self.settingsButton.pack(pady=5, padx=5)
 
-        getDataButton = ttk.Button(menuFrame, text="Get Data")
-        getDataButton.pack(padx=5, pady=5)
+        self.getDataButton = ttk.Button(self.menuFrame, text="Get Data")
+        self.getDataButton.pack(padx=5, pady=5)
 
-        exportButton = ttk.Button(menuFrame, text="Export")
-        exportButton.pack(padx=5, pady=5)
+        self.exportButton = ttk.Button(self.menuFrame, text="Export")
+        self.exportButton.pack(padx=5, pady=5)
 
-        closeButton = ttk.Button(master, text="Exit", command=exit)
-        closeButton.place(x=15, y=240)
+        self.closeButton = ttk.Button(master, text="Exit", command=self.exitApp)
+        self.closeButton.place(x=15, y=240)
 
         # creating the display area text and its content
-        dispAreaFrame = ttk.Frame(master, width=385, border=5, relief=SOLID)
-        dispAreaFrame.place(x=115, y=15)
+        self.dispAreaFrame = ttk.Frame(master, width=385, border=5, relief=SOLID)
+        self.dispAreaFrame.place(x=115, y=15)
 
         welcomeMsg = "This project generates a set of random data based on the user input, stores the result and facilitates its export. \n\n It has been built using the Python Standard Library and the Tkinter modules"
-        displayArea = ttk.Label(
-            dispAreaFrame,
+        self.dispAreaLabel = ttk.Label(
+            self.dispAreaFrame,
             text=welcomeMsg,
             justify="left",
             background="yellow",
             wraplength=375,
         )
-        displayArea.pack(fill="both")
+        self.dispAreaLabel.pack()
 
-    def exit():
-        root.destroy()
+    def userInputLayout(self):
+        #     # hides the display label and inserts the widgets
+        #     # to collect the user input
+        self.dispAreaLabel.forget()
+
+    def exitApp(self):
+        exit()
 
 
 def main():
+    # Generating the GUI and setting up its properties
     pass
 
 
