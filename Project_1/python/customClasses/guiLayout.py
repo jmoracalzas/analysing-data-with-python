1b9344b4eb9c8fce2a500f6bb6acced1d9b59402
Jordi Mora Calzas <jordi.mora.calzas@outlook.com>
2020-09-19 11:36:37 +0100

    Testing import statements

--- Project_1/python/customClasses/guiLayout.py
+++ Project_1/python/customClasses/guiLayout.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+from sys import path
+
+path.append("./Project_1/python/customClasses")
 from dataGenClasses import Rules, interimData
 
 
